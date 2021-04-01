59880e8cc8e0b3ef6c4570d7cb509532b63dabef
Abiel Ortega <abielortega90@gmail.com>
2021-04-01 13:18:48 -0500

    added a practcie array lol

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var family = ["Liliana", "Noe", "Jonadab", "Jeroham", "Eden", "Abiel"]
+const last_Name = " Ortega"; 
+
+var me = family[5] + last_Name;
+var mom = family[0] + last_Name;
+var dad = family[1] + last_Name;
+var brother = family[2] + ", " + family[3] + last_Name;
+var sister = family[4] + last_Name;
+
+console.log("Hi my name is " + me + ". My mother is " + mom + " and my father is " + dad + ". I have 3 siblings, 2 brothers which are " + brother + " and one sister which is " + sister + ". This is my family that I have grown up knowing all my life together up to now.");
